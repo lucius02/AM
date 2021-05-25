2d162956738af47343eb11da6453d6417a497957
lucius02 <54938206+lucius02@users.noreply.github.com>
2021-05-25 14:41:57 +0200

    Afspraak maken

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { Slides } from 'ionic-angular/umd';
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
+  templateUrl: `tab2.page.html`,
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
 
+export class SlideExample {
+  // Optional parameters to pass to the swiper instance.
+  // See http://idangero.us/swiper/api/ for valid options.
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    allowTouchMove: false
+  };
   constructor() {}
-
 }
